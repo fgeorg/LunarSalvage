d96253ecbcbf46e4f639521e8d764fc8b5625960
Francesco Georg <francesco.georg@wooga.com>
2014-07-09 16:23:10 +0200

    trying to get android to work. removed some ignored files

--- Assets/Follower.cs
+++ Assets/Follower.cs
@@ -17,21 +17,21 @@ public class Follower : MonoBehaviour {
 			Vector3 relativePosition = transform.position - mousePosition;
 			Vector3 force = new Vector3(relativePosition.x, relativePosition.y, 0);
 			rigidbody2D.AddForce(force);
-			print(mousePosition + " " + force);
+//			print(mousePosition + " " + force);
 
 
 //			transform.position = new Vector3(mousePosition.x, mousePosition.y, 0);
 		}
 	}
 
-//	// Prints number of fingers touching the screen
-//	function Update () {
-//		var fingerCount = 0;
-//		for (Touch touch in Input.touches) {
-//			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
-//				fingerCount++;
-//		}
-//		if (fingerCount > 0)
-//			print ("User has " + fingerCount + " finger(s) touching the screen");
-//	}
+	// Prints number of fingers touching the screen
+	void Update () {
+		var fingerCount = 0;
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+				fingerCount++;
+		}
+		if (fingerCount > 0)
+			print ("User has " + fingerCount + " finger(s) touching the screen");
+	}
 }
