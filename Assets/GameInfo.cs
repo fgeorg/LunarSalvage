f6db5845da15f2c433b4fbf48bc086ad4f83d458
Huel Fuchsberger <huel.fuchsberger@wooga.com>
2014-07-09 18:37:18 +0200

    Add basic win and lose conditions

---
+++ Assets/GameInfo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInfo : MonoBehaviour {
+
+	[SerializeField] public int gameStatus = 0; //0 Paused,1 Running,2 Won, 3 Lost
+
+	// Use this for initialization
+	void Start () 
+	{
+		gameStatus = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Images/SavedObjectTrigger.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SavedObjectTrigger : MonoBehaviour
+{
+	[SerializeField] public GameInfo gameInfo;
+
+	void OnStart()
+	{
+	
+	}
+
+
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		Debug.Log("HIT!");
+
+		if(other.gameObject.tag == "SavedObject")
+		{
+
+			gameInfo.gameStatus = 2;	
+		}
+	}
+
+	void OnGUI () {
+		// Make a background box
+
+		if (gameInfo.gameStatus == 2) {
+				
+			GUI.Box(new Rect(10,10,100,90), "You Won!");
+
+			if(GUI.Button(new Rect(20,40,80,20), "Restart")) {
+				//Application.LoadLevel(1);
+			}
+
+			if(GUI.Button(new Rect(20,70,80,20), "Quit")) {
+				//Application.LoadLevel(2);
+			}
+		}
+
+
+	}
+}
