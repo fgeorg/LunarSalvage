a4b4999ace46a38d7fd28391acda1867af0563f0
charlottevorbeck <charlotte.vorbeck@wooga.net>
2014-07-09 12:51:41 +0200

    Simple follow

--- Assets/Follower.cs
+++ Assets/Follower.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Follower : MonoBehaviour {
+	private Vector3 mousePosition;
+	private float moveSpeed = 0.1f;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +11,9 @@ public class Follower : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		mousePosition = Input.mousePosition;
+		mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+		Debug.Log("mouse pos: " + Input.mousePosition.x + Input.mousePosition.y);
+		transform.position = new Vector3(mousePosition.x, mousePosition.y, 0);//Vector3.Lerp(transform.position, mousePosition, moveSpeed);
 	}
 }
