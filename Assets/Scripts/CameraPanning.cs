6ccfaf223c0b78b02de5007f59e175e1aa83602c
Tom Kinniburgh <thomas.kinniburgh@wooga.net>
2014-07-10 11:05:27 +0200

    Adding level 2 and making it default.  Also adjusted some values in the camera panning.

--- Assets/Scripts/CameraPanning.cs
+++ Assets/Scripts/CameraPanning.cs
@@ -12,14 +12,14 @@ public class CameraPanning : MonoBehaviour {
 	void FixedUpdate () {
 		if (player == null) return;
 		Vector2 curPos = new Vector2(camera.transform.position.x, camera.transform.position.y);
-		Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y - 5);
+		Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y - 3);
 		Vector2 diff = playerPos - curPos;
 		if (diff.sqrMagnitude > 2.0f * 2.0f) {
-			Vector2 toMove = diff * .01f;
+			Vector2 toMove = diff * .02f;
 			camera.transform.position = new Vector3(curPos.x + toMove.x,
 			                                        curPos.y + toMove.y,
 			                                        camera.transform.position.z);
-			float targetSize = 12 + 2 * player.rigidbody2D.velocity.magnitude;
+			float targetSize = 12 + (3 * player.rigidbody2D.velocity.magnitude);
 			camera.orthographicSize = camera.orthographicSize * .99f + targetSize * .01f;
 		}
 
