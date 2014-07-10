6add0cbf9e1010a19aae9e915d0177e1feb57b40
Francesco Georg <francesco.georg@wooga.com>
2014-07-10 15:57:49 +0200

    added background

--- Assets/Scripts/CameraPanning.cs
+++ Assets/Scripts/CameraPanning.cs
@@ -14,7 +14,7 @@ public class CameraPanning : MonoBehaviour {
 		Vector2 curPos = new Vector2(camera.transform.position.x, camera.transform.position.y);
 		Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y - 3);
 		Vector2 diff = playerPos - curPos;
-		if (diff.sqrMagnitude > 2.0f * 2.0f) {
+		if (diff.sqrMagnitude > 0) {
 			Vector2 toMove = diff * .02f;
 			camera.transform.position = new Vector3(curPos.x + toMove.x,
 			                                        curPos.y + toMove.y,
