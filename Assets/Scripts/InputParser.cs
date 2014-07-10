0f6a544d34c1199a100966d16f6f654e08ee1d82
Francesco Georg <francesco.georg@wooga.com>
2014-07-10 11:38:36 +0200

    adding two button thruster behavior

---
+++ Assets/Scripts/InputParser.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class InputParser : MonoBehaviour {
+	public float leftThrust;
+	public float rightThrust;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A))
+		{
+			leftThrust = 1;
+		}
+		if (Input.GetKeyUp (KeyCode.A))
+		{
+			leftThrust = 0;
+		}
+		if (Input.GetKeyDown (KeyCode.D))
+		{
+			rightThrust = 1;
+		}
+		if (Input.GetKeyUp (KeyCode.D))
+		{
+			rightThrust = 0;
+		}
+	}
+
+	// Prints number of fingers touching the screen
+//	void Update () {
+//		var fingerCount = 0;
+//		foreach (Touch touch in Input.touches) {
+//			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+//				fingerCount++;
+//		}
+//		if (fingerCount > 0)
+//			print ("User has " + fingerCount + " finger(s) touching the screen");
+//	}
+}
--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -3,20 +3,34 @@ using System.Collections;
 
 public class SpaceshipController : MonoBehaviour {
 
-	[SerializeField] public GameInfo gameInfo;
-	[SerializeField] public GameObject explosionPrefab;
+	public GameInfo gameInfo;
+	public GameObject explosionPrefab;
+	public InputParser inputParser;
+	public ThrusterRenderer leftThruster;
+	public ThrusterRenderer rightThruster;
+
 	void OnStart()
 	{
 		
 	}
-	
+
+	void FixedUpdate() {
+		Vector3 force = new Vector3();
+		if (inputParser.leftThrust > 0)
+		{
+			force += new Vector3(10, 20, 0);
+		}
+		if (inputParser.rightThrust > 0)
+		{
+			force += new Vector3(-10, 20, 0);
+		}
+		rigidbody2D.AddForce(force);
+	}
+
 	void OnCollisionEnter2D (Collision2D other)
 	{
 		gameInfo.gameStatus = 3;	
-//		ParticleSystem ps = GetComponent<ParticleSystem>();
-//		ps.Play ();
 		Instantiate(explosionPrefab, transform.position, transform.rotation);
-//		explosion.transform.position = gameObject.transform.position;
 		Destroy (gameObject);
 	}
 
