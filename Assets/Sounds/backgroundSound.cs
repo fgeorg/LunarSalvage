034443dd08781fb293e36aff6085568e2a2d7811
Tom Kinniburgh <thomas.kinniburgh@wooga.net>
2014-07-10 17:10:10 +0200

    Adding sound script for the background sound

---
+++ Assets/Sounds/backgroundSound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundSound : MonoBehaviour {
+
+
+	public AudioSource au_background ;
+
+	// Use this for initialization
+	void Start () {
+
+		//au_background = this.gameObject.GetComponent("AudioSource");
+		au_background.loop = true;
+		au_background.Play();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
