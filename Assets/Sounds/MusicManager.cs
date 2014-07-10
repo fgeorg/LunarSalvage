fec984b718aa80b076860af674716cd7b3999128
Huel Fuchsberger <huel.fuchsberger@wooga.com>
2014-07-10 18:27:24 +0200

    Sound looper

---
+++ Assets/Sounds/MusicManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+
+	public AudioSource currentClip;
+	public AudioSource nextClip;
+
+	public GameInfo gameinfo;
+
+	private AudioSource[] audioList; 
+
+	public float timer;
+	public float nextClipTime;
+
+	private bool stayInLoop = false;
+
+	// Use this for initialization
+	void Start () {
+
+		//au_background = this.gameObject.GetComponent("AudioSource");
+		currentClip.loop = true;
+		currentClip.Play();
+
+		audioList = this.gameObject.GetComponents<AudioSource>();
+		nextClipTime = (float)currentClip.clip.length;
+
+		checkNextClip();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(stayInLoop == true) return;
+
+		timer += Time.deltaTime;
+	
+		if(timer >= nextClipTime)
+		{
+			Debug.Log("Next Clip Playing!");
+
+			currentClip.Stop ();
+			currentClip = nextClip;
+			currentClip.Play ();
+			
+			checkNextClip();
+
+			timer = 0;
+		}
+	
+	
+	}
+
+	private void checkNextClip()
+	{
+		
+		switch (currentClip.clip.name) {
+		case "intro-loop":
+			
+			
+			foreach (AudioSource item in audioList) {
+				if (item.clip.name == "transition") {
+					
+					nextClip = item;
+				}
+				
+			}
+			
+			break;
+			
+		case "transition":
+			
+			foreach (AudioSource item in audioList) {
+				if (item.clip.name == "main-loop") {
+					
+					nextClip = item;
+	 
+				}
+				
+			}
+			
+			
+			break;
+			
+		case "main-loop":
+			
+			stayInLoop = true;
+
+			break;
+		}
+
+	}
+}
