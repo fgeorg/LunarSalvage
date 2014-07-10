1b230cd98da04ea44b61316563dcfc7a9a084003
charlottevorbeck <charlotte.vorbeck@wooga.net>
2014-07-10 13:49:44 +0200

    First pass

--- Assets/Scripts/ThrusterRenderer.cs
+++ Assets/Scripts/ThrusterRenderer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ThrusterRenderer : MonoBehaviour {
 	public float thrust;
 	public SpriteRenderer fireSprite;
+	public ParticleSystem particles;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,6 +12,10 @@ public class ThrusterRenderer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (thrust > 0)
+		{
+			particles.Emit(5);
+		}
 		fireSprite.renderer.enabled = thrust > 0;
 	}
 }
