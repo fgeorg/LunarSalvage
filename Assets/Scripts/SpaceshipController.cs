af95ce563ad91eee43cc1dfc81bcd3ad63d66513
Huel Fuchsberger <huel.fuchsberger@wooga.com>
2014-07-10 11:52:07 +0200

    Added Assets and TakeOffZones

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -3,17 +3,25 @@ using System.Collections;
 
 public class SpaceshipController : MonoBehaviour {
 
+<<<<<<< Updated upstream
 	public GameInfo gameInfo;
 	public GameObject explosionPrefab;
 	public InputParser inputParser;
 	public ThrusterRenderer leftThruster;
 	public ThrusterRenderer rightThruster;
+=======
+	[SerializeField] public GameInfo gameInfo;
+	[SerializeField] public GameObject explosionPrefab;
+
+	[SerializeField] private bool inTakeOffZone = true;
+>>>>>>> Stashed changes
 
 	void OnStart()
 	{
 		
 	}
 
+<<<<<<< Updated upstream
 	void FixedUpdate() {
 		Vector3 force = new Vector3();
 		if (inputParser.leftThrust > 0)
@@ -27,11 +35,42 @@ public class SpaceshipController : MonoBehaviour {
 		rigidbody2D.AddForce(force);
 	}
 
+=======
+	void OnTriggerEnter2D (Collider2D other) {
+		
+		if (other.gameObject.name == "TakeOffZone" && inTakeOffZone != true) 
+		{
+			Debug.Log("Entered SaveZone");
+			inTakeOffZone = true; 
+			return;
+		} 
+
+		
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		
+		if (other.gameObject.name == "TakeOffZone" && inTakeOffZone != false) 
+		{ 
+			Debug.Log("Exited SaveZone");
+			inTakeOffZone = false; 
+			return;
+		} 
+		
+	}
+
+	
+>>>>>>> Stashed changes
 	void OnCollisionEnter2D (Collision2D other)
 	{
+
+		if (inTakeOffZone == true) return;
+
 		gameInfo.gameStatus = 3;	
 		Instantiate(explosionPrefab, transform.position, transform.rotation);
 		Destroy (gameObject);
 	}
+
+
 
 }
