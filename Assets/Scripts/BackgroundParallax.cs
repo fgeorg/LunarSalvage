4d275363462e8a173c303a91e076ca2f9892e202
Francesco Georg <francesco.georg@wooga.com>
2014-07-11 12:11:12 +0200

    added background parallax

--- Assets/Scripts/BackgroundParallax.cs
+++ Assets/Scripts/BackgroundParallax.cs
@@ -2,16 +2,24 @@
 using System.Collections;
 
 public class BackgroundParallax : MonoBehaviour {
-	public float parallaxFactor;
+	public Vector2 parallaxFactor;
+	public Ferr2DT_PathTerrain terrain;
+//	private Ferr2D_Path path;
+	private Vector2 startPos;
+	private Vector2 startCamPos;
+	private GameObject mainCamera;
 	// Use this for initialization
 	void Start () {
-		transform.localScale = new Vector3(8.0f, 8.0f, 1.0f);
+		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera");
+		startCamPos = new Vector2 (mainCamera.transform.position.x, mainCamera.transform.position.y);
+		startPos = new Vector2 (transform.position.x, transform.position.y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
-		Vector3 targetPosition = camera.transform.position * (1 + parallaxFactor);
-		transform.position = new Vector3 (targetPosition.x, targetPosition.y, transform.position.z);
+		Vector2 camPosition = new Vector2 (mainCamera.transform.position.x, mainCamera.transform.position.y);
+		Vector2 diff = camPosition - startCamPos;
+		transform.position = new Vector3 (startPos.x + parallaxFactor.x * diff.x, startPos.y + parallaxFactor.y * diff.y, transform.position.z);
+		terrain.RecreatePath ();
 	}
 }
